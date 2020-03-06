7ed492f70d6cfce540840d78bec796d09d733ac7
Scott Key <scottdkey@gmail.com>
2020-03-05 23:08:42 -0700

    stuck and trying to find a way out - to continue work 3/6

--- fc.js
+++ fc.js
@@ -16,18 +16,46 @@ let blankCard = {
   points: 2
 };
 const cardCont = document.getElementById("cards_container");
+const editForm = document.createElement("form");
+const editBtn = document.createElement("button");
+
 const buildCard = c => {
   //info needed to build a card.
   let card = document.createElement("div");
-  card.setAttribute("class", "card");
-  let id = cards.length;
+  card.classList.add("card");
+  let id = Math.random();
   card.setAttribute("id", id);
   let cardTitle = c.title;
   let cardBody = c.body;
-  card.innerHTML = `<div class='card_title'>${cardTitle}</div><button>edit</button>
-                    <div class='card_body'>${cardBody}</div><button>edit</button>
-                    <button>delete card</button>`;
+
+  //what the card will look like
+  card.innerHTML = `<h3 class='card_title'>${cardTitle}</h3>
+                    <div class='card_body'>${cardBody}</div>`;
+  // info needed for edit form
+
+  //created form to edit
+
+  editForm.classList.add("edit-form");
+  editForm.classList.add("hidden");
+  editForm.innerHTML = `<label for="title">Card Title</label>
+                      <input id="title" type="text" />
+                      
+                      <label for="body">Card Body</label>
+                      <input id="body" type="text" />`;
+
+  //create edit button
+  editBtn.innerText = "edit";
+  editBtn.classList.add("edit-btn");
+
+  //final card build and push to page
+  card.append(editBtn);
+  card.append(editForm);
   return cardCont.appendChild(card);
+};
+
+const editCard = card => {
+  const editForm = document.querySelector("edit-form" + card.id);
+  editForm.classList.toggle("hidden");
 };
 
 const addCard = e => {
@@ -57,6 +85,8 @@ const addCard = e => {
   newCardPoints.value = "";
 };
 
+// edit form for each card
+
 const cardsOnLoad = arrayOfObjects => {
   arrayOfObjects.forEach(card => {
     buildCard(card);
@@ -75,6 +105,11 @@ const cardsOnLoad = arrayOfObjects => {
 const button = document.querySelector('button[type="submit"]');
 button.addEventListener("click", addCard);
 
+editBtn.addEventListener("click", editCard(element.id));
+
 // newCard.addEventListener("click", () => addCard(blankCard));
 
 cardsOnLoad(cards);
+
+// I'm super suck and need to get some sleep. Things are broken and i'll try to fix them in the morning and finish stuff up
+//crud isn't working and the stuff I'm finding online gets me close but isn't working.
